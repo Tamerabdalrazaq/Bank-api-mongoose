ca1877748ed058ca1f30e60c3efe03833d080a9e
Tamer <tamerabdalrazaq@hotmail.com>
2021-04-18 09:23:21 +0300

    v1-comments

--- server.js
+++ server.js
@@ -9,3 +9,10 @@ app.use(express.urlencoded({extended: false}));
 app.use('/api/users', require('./routes/users'))
 
 app.listen(3000, () => console.log('Server is up and running'))
+
+
+// requests examples: 
+// GET: http://localhost:3000/api/users OR http://localhost:3000/api/users/:id
+// POST: http://localhost:3000/api/users, Request body: {"id": "123456789" [, "cash": 0, "credit": 0]}
+// PUT: http://localhost:3000/api/users/:id?action=['withdraw', 'deposit', 'updateCredit']
+// PUT: http://localhost:3000/api/users/:id(from)?action=transfer, Request body: {"id"(to): "123456789", "cash": 0}
